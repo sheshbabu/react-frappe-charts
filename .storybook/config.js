c50f5a7c55fa33bd3b58b6511c5fff088b8e4c0e
Sheshbabu <sheshbabu@redmart.com>
2019-09-22 11:18:52 +0800

    Updated storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,14 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator, addParameters } from "@storybook/react";
+import centered from "@storybook/addon-centered/react";
+
+addDecorator(centered);
+
+addParameters({
+  options: {
+    showPanel: false,
+    panelPosition: "right"
+  }
+});
 
 function loadStories() {
   const req = require.context("../src", true, /\.story\.js$/);
