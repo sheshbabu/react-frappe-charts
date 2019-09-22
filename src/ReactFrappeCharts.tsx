fb6f088128a3f60bbb04fb193472cec7eb44b67f
Sheshbabu <sheshbabu@redmart.com>
2019-09-22 11:18:15 +0800

    Improved type coverage

--- src/ReactFrappeCharts.tsx
+++ src/ReactFrappeCharts.tsx
@@ -1,21 +1,62 @@
 import React from "react";
 import { Chart } from "frappe-charts/dist/frappe-charts.min.esm";
 
+type ChartType =
+  | "line"
+  | "bar"
+  | "axis-mixed"
+  | "pie"
+  | "percentage"
+  | "heatmap";
+
+type AxisMode = "span" | "tick";
+
+type ChartData = {
+  labels: Array<string>;
+  datasets: Array<{
+    name?: string;
+    type?: ChartType;
+    values: Array<number>;
+  }>;
+  dataPoints?: { ["string"]: number };
+  start?: Date;
+  end?: Date;
+};
+
 type Props = {
-  type: "line" | "bar" | "axis-mixed" | "pie" | "percentage" | "heatmap";
-  data: {
-    labels: Array<string>;
-    datasets: Array<{
-      values: Array<number>;
-    }>;
-  };
-  colors: Array<string>;
-  height: number;
-  axisOptions: {
-    xAxisMode: "span" | "tick";
-    yAxisMode: "span" | "tick";
+  title?: string;
+  type?: ChartType;
+  data: ChartData;
+  height?: number;
+  colors?: Array<string>;
+  axisOptions?: {
+    xAxisMode: AxisMode;
+    yAxisMode: AxisMode;
     xIsSeries: 0 | 1;
   };
+  barOptions?: {
+    spaceRatio: number;
+    stacked: 0 | 1;
+    height: number;
+    depth: number;
+  };
+  lineOptions?: {
+    dotSize: number;
+    hideLine: 0 | 1;
+    hideDots: 0 | 1;
+    heatline: 0 | 1;
+    regionFill: number;
+    areaFill: number;
+  };
+  isNavigable?: boolean;
+  maxSlices?: number;
+  addDataPoint: (
+    label: string,
+    valueFromEachDataset: Array<number>,
+    index: number
+  ) => void;
+  removeDataPoint: (index: number) => void;
+  update: (data: ChartData) => void;
 };
 
 export default function ReactFrappeCharts(props: Props) {
