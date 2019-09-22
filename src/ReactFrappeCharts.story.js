38ad4cb3a09404d301b3b9d75d36d7a2e7b94691
Sheshbabu <sheshbabu@redmart.com>
2019-09-22 11:18:27 +0800

    Added stories for all chart types

--- src/ReactFrappeCharts.story.js
+++ src/ReactFrappeCharts.story.js
@@ -1,52 +1,27 @@
 import React from "react";
+import addDays from "date-fns/addDays";
+import isBefore from "date-fns/isBefore";
+import isEqual from "date-fns/isEqual";
 import { storiesOf } from "@storybook/react";
 import ReactFrappeCharts from "./ReactFrappeCharts";
 
-storiesOf("Bar", module)
-  .add("Default", () => (
-    <div style={{ width: "500px" }}>
-      <ReactFrappeCharts
-        type="bar"
-        colors={["red"]}
-        axisOptions={{ xAxisMode: "tick", yAxisMode: "tick", xIsSeries: 1 }}
-        height={300}
-        data={{
-          labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-          datasets: [{ values: [18, 40, 30, 35, 8, 52, 17, -4] }]
-        }}
-      />
-    </div>
-  ))
-  .add("More Datasets", () => (
-    <div style={{ width: "500px" }}>
-      <ReactFrappeCharts
-        type="bar"
-        colors={["red"]}
-        axisOptions={{ xAxisMode: "tick", yAxisMode: "tick", xIsSeries: 1 }}
-        height={300}
-        data={{
-          labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-          datasets: [
-            { name: "Dataset 1", values: [18, 40, 30, 35, 8, 52, 17, -4] },
-            { name: "Dataset 2", values: [30, 50, -10, 15, 18, 32, 27, 14] }
-          ]
-        }}
-      />
-    </div>
-  ));
+const config = {
+  colors: ["#21ba45", "#98d85b"],
+  axisOptions: { xAxisMode: "tick", yAxisMode: "tick", xIsSeries: 1 },
+  height: 300
+};
+const labels = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
 storiesOf("Line", module)
   .add("Line", () => (
     <div style={{ width: "500px" }}>
       <ReactFrappeCharts
         type="line"
-        colors={["#21ba45"]}
-        axisOptions={{ xAxisMode: "tick", yAxisMode: "tick", xIsSeries: 1 }}
-        height={300}
         data={{
-          labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+          labels: labels,
           datasets: [{ values: [18, 40, 30, 35, 8, 52, 17, 4] }]
         }}
+        {...config}
       />
     </div>
   ))
@@ -54,16 +29,145 @@ storiesOf("Line", module)
     <div style={{ width: "500px" }}>
       <ReactFrappeCharts
         type="line"
-        colors={["#21ba45"]}
-        axisOptions={{ xAxisMode: "tick", yAxisMode: "tick", xIsSeries: 1 }}
-        height={300}
         data={{
-          labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+          labels: labels,
           datasets: [
             { name: "Dataset 1", values: [18, 40, 30, 35, 8, 52, 17, -4] },
             { name: "Dataset 2", values: [30, 50, -10, 15, 18, 32, 27, 14] }
           ]
         }}
+        {...config}
       />
     </div>
   ));
+
+storiesOf("Bar", module)
+  .add("Default", () => (
+    <div style={{ width: "500px" }}>
+      <ReactFrappeCharts
+        type="bar"
+        data={{
+          labels: labels,
+          datasets: [{ values: [18, 40, 30, 35, 8, 52, 17, -4] }]
+        }}
+        {...config}
+      />
+    </div>
+  ))
+  .add("More Datasets", () => (
+    <div style={{ width: "500px" }}>
+      <ReactFrappeCharts
+        type="bar"
+        data={{
+          labels: labels,
+          datasets: [
+            { name: "Dataset 1", values: [18, 40, 30, 35, 8, 52, 17, -4] },
+            { name: "Dataset 2", values: [30, 50, -10, 15, 18, 32, 27, 14] }
+          ]
+        }}
+        {...config}
+      />
+    </div>
+  ));
+
+storiesOf("Mixed Axis", module).add("Default", () => (
+  <div style={{ width: "500px" }}>
+    <ReactFrappeCharts
+      type="axis-mixed"
+      data={{
+        labels: labels,
+        datasets: [
+          {
+            name: "Dataset 1",
+            values: [18, 40, 30, 35, 8, 52, 17, -4],
+            chartType: "bar"
+          },
+          {
+            name: "Dataset 2",
+            values: [30, 50, -10, 15, 18, 32, 27, 14],
+            chartType: "line"
+          }
+        ]
+      }}
+      {...config}
+    />
+  </div>
+));
+
+storiesOf("Pie", module).add("Default", () => (
+  <div style={{ width: "500px" }}>
+    <ReactFrappeCharts
+      type="pie"
+      data={{
+        labels: labels,
+        datasets: [
+          {
+            name: "Dataset 1",
+            values: [18, 40, 30, 35, 8, 52, 17, -4],
+            chartType: "bar"
+          },
+          {
+            name: "Dataset 2",
+            values: [30, 50, -10, 15, 18, 32, 27, 14],
+            chartType: "line"
+          }
+        ]
+      }}
+      {...config}
+    />
+  </div>
+));
+
+storiesOf("Percentage", module).add("Default", () => (
+  <div style={{ width: "500px" }}>
+    <ReactFrappeCharts
+      type="percentage"
+      data={{
+        labels: labels,
+        datasets: [
+          {
+            name: "Dataset 1",
+            values: [18, 40, 30, 35, 8, 52, 17, -4],
+            chartType: "bar"
+          },
+          {
+            name: "Dataset 2",
+            values: [30, 50, -10, 15, 18, 32, 27, 14],
+            chartType: "line"
+          }
+        ]
+      }}
+      {...config}
+    />
+  </div>
+));
+
+const startDate = new Date("2018-09-01");
+const endDate = new Date("2018-12-31");
+
+function getHeatMapData() {
+  let dataPoints = {};
+  let currDate = startDate;
+  while (isBefore(currDate, endDate) || isEqual(currDate, endDate)) {
+    const count = pickRandomNumber(10, 1000);
+    dataPoints[currDate.getTime() / 1000] = count;
+    currDate = addDays(currDate, 1);
+  }
+  return dataPoints;
+}
+
+function pickRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+storiesOf("Heatmap", module).add("Default", () => (
+  <ReactFrappeCharts
+    type="heatmap"
+    data={{
+      dataPoints: getHeatMapData(),
+      start: startDate,
+      end: endDate
+    }}
+    {...config}
+  />
+));
